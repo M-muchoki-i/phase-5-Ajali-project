e4002745a82d0b6ed502b9ce667ac0c8eb00ccda
eugene <kapombek@gmail.com>
2025-07-17 10:28:22 +0300

    added axios

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,13 +6,21 @@ import {
 } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import { SignupForm } from './Pages/SignUp.jsx';
+import LocationPage from './components/locationMap.jsx';
+import EmergencyContact from './pages/emergencycontacts.jsx';
+import { User } from './pages/User.jsx';
+import { LoginForm } from './Pages/Login.jsx';
+import ReportForm from './pages/report-form.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <SignupForm/>,
+    element: <ReportForm />,
   },
+  {
+    path: "/login",
+    element: <LoginForm />
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
