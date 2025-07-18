b4c00bb98d3ccf83498e74c2442632ea9e8c326f
eugene <kapombek@gmail.com>
2025-07-18 13:17:14 +0300

    added GET and POST for Report resource

---
+++ Backend/resources/report.py
@@ -0,0 +1,35 @@
+from flask-restful import Resource, reqparse
+from models import db, Report
+from sqlalchemy.exc import SQLAlchemyError
+
+class ReportResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('incident', type=str, required=True, help='Incident type is reqired')
+    parser.add_argument('Description', type=str, help='Please provide a description')
+    parser.add_argument('Latitude',required=True,  type=float, help='Latitude not provided')
+    parser.add_argument('Longitude', required=True, type=float, help='Longitude not provided')
+    parser.add_argument('Media', type=str, help='Media not attached')
+
+
+    def post(self):
+        data = ReportResource.parser.parse_args()
+
+        try:
+            report = Report(**data)
+            db.session.add(report)
+            db.session.commit()
+            return report.to_dict(), 201
+        except Exception as e:
+            return {'message': str(e)}, 400
+        except SQLAlchemyError:
+            db.session.rollback()
+            return {'message':'Database error'}, 500
+
+    def get(self, report_id=None):
+        if report_id:
+            report = Report.query.get(report_id)
+            if report:
+                return report.to_dict()
+            
+        reports = Report.query.all()
+        return [r.to_dict() for r in reports]
