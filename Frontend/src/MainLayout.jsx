ba09cad5dc0e914a63d76b35d87abb1602c9f0ea
stephen <stephenmagiya97@gmail.com>
2025-07-24 12:04:25 +0300

    creates layout to appench children

---
+++ Frontend/src/MainLayout.jsx
@@ -0,0 +1,11 @@
+// src/layouts/MainLayout.jsx
+import Footer from "../pages/Footer";
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="pb-28">
+      {children}
+      <Footer />
+    </div>
+  );
+}
