b328255624ceef8b3eea5e6afa05cf6f0d7b3fb0
eugene <kapombek@gmail.com>
2025-07-25 13:59:06 +0300

    Made initial layout of map page and its elements

--- Frontend/src/components/MapPage.jsx
+++ Frontend/src/components/MapPage.jsx
@@ -1,6 +1,12 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-export default function MapPage() {
+export default function MapPage({ position,
+    setPosition,
+    loading,
+    error,
+    onLocate, }) {
     // this instantiates the map initial state for reference as a mutable variable
     const mapRef = useRef();
 
@@ -11,6 +17,27 @@ export default function MapPage() {
     },[position]) //position as a dependancy for the side-effect
 
     return (
-        <></>
+        //first fragment is a container for the entire page
+        <>
+            <div>
+                {/*conditional render for loading states  will do here*/}
+            
+                <div> {/*div for button styling*/}
+                <button onClick={onLocate} title="Find my location"></button> {/*button will only carry and svg image from online for better appearance, no text*/}
+                </div>
+                {/*imported map components from react leaflet will enter here*/}
+                <MapContainer>
+                    <TileLayer />
+                </MapContainer>
+
+                {/*box to show coordinates at corner of the page layout */}
+                {position && (
+                    <div>
+                        <div>Cordinates</div>
+                        <div>{position.lat.toFixed(5)},{position.lng.toFixed(5)}</div>
+                    </div>
+                ) }
+            </div>
+        </>
     )
 }
