e9bdc93f89dab38913704d395470fdf7607c86e1
eugene <kapombek@gmail.com>
2025-07-30 20:41:01 +0300

    WIP: Cleaning up file structure

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -19,12 +19,6 @@ export function Login() {
 
       const res = await fetch(`${API_BASE_URL}/login`, {
 
-
-      const res = await fetch(`${API_BASE_URL}/login`, {
-
-      const res = await fetch("http://127.0.0.1:5000/login", {
-
-
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
